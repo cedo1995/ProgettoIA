9edd04b5de5e94150f556595611dc25dbee2af8d
Unknown <roberto.cedo@gmail.com>
2018-04-23 16:05:44 +0200

    User system recognizing in Main

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> paths = new ArrayList<>();
-        paths.add("C:/Users/Paolo/Downloads/a_example.in");
-        paths.add("C:/Users/Paolo/Downloads/b_should_be_easy.in");
-        paths.add("C:/Users/Paolo/Downloads/c_no_hurry.in");
-        paths.add("C:/Users/Paolo/Downloads/d_metropolis.in");
-        paths.add("C:/Users/Paolo/Downloads/e_high_bonus.in");
+        String[] user=new String[2];
+        user[0]="Paolo";
+        user[1]="cedo";
+        if (user[0].equals(System.getProperty("user.name"))){
+            paths.add("C:/Users/Paolo/Downloads/a_example.in");
+            paths.add("C:/Users/Paolo/Downloads/b_should_be_easy.in");
+            paths.add("C:/Users/Paolo/Downloads/c_no_hurry.in");
+            paths.add("C:/Users/Paolo/Downloads/d_metropolis.in");
+            paths.add("C:/Users/Paolo/Downloads/e_high_bonus.in");
+
+        }else if(user[1].equals(System.getProperty("user.name"))){
+            paths.add("/home/cedo/Downloads/IA/a_example.in");
+            paths.add("/home/cedo/Downloads/IA/b_should_be_easy.in");
+            paths.add("/home/cedo/Downloads/IA/c_no_hurry.in");
+            paths.add("/home/cedo/Downloads/IA/d_metropolis.in");
+            paths.add("/home/cedo/Downloads/IA/e_high_bonus.in");
+        }
 
         int tot = 0;
 
